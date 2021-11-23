2f2537809b47db073a48475480818564120ef350
Matthias Schilder <matthias.schilder1@googlemail.com>
2021-11-23 15:42:03 +0100

    initial commit

---
+++ wedit/WorkflowConnector.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wedit
+{
+    /// <summary>
+    /// Interaction logic for WorkflowConnector.xaml
+    /// </summary>
+    public partial class WorkflowConnector : Button
+    {
+        public WorkflowConnector()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ wedit/WorkflowItem3.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wedit
+{
+    /// <summary>
+    /// Interaction logic for WorkflowItem3.xaml
+    /// </summary>
+    public partial class WorkflowItem3 : UserControl
+    {
+        public ProcessorType ProcessorType { get; set; }
+
+        public bool HasInputConnector
+        {
+            get
+            {
+                if (ProcessorType == ProcessorType.InputOnly || ProcessorType == ProcessorType.InputAndOutput)
+                    return true;
+                return false;
+            }
+        }
+        public bool HasOutputConnector
+        {
+            get
+            {
+                if (ProcessorType == ProcessorType.OutputOnly || ProcessorType == ProcessorType.InputAndOutput)
+                    return true;
+                return false;
+            }
+        }
+
+        public WorkflowItem3()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
